ee84305a1862331e5abaf7594f9ffa6ca4b10479
Demorome <69116996+Demorome@users.noreply.github.com>
2025-02-16 22:19:10 -0400

    remove useless Wall component

--- src/Components/Components.cs
+++ src/Components/Components.cs
@@ -27,7 +27,6 @@ public readonly record struct IntegerVelocity(Vector2 Value);
 public readonly record struct Solid();
 public readonly record struct TailPartBecomeActiveNextMovement();
 //public readonly record struct TouchingSolid();
-public readonly record struct Wall();
 public readonly record struct CanBeGrabbed();
 public readonly record struct GrowsPlayerOnPickup();
 
