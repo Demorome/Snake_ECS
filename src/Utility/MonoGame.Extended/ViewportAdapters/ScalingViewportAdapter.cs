b58dc62267250c5fe1d2412cf6e66d791bad4e75
demorome <demoromestuff@gmail.com>
2026-01-05 04:19:41 -0400

    add GetScale option for debugging

--- src/Utility/MonoGame.Extended/ViewportAdapters/ScalingViewportAdapter.cs
+++ src/Utility/MonoGame.Extended/ViewportAdapters/ScalingViewportAdapter.cs
@@ -11,11 +11,17 @@ namespace MonoGame.Extended.ViewportAdapters
             : base(window)
         { }
 
+        public Vector2 GetScale()
+        {
+            return new Vector2(
+                (float)ViewportWidth / GameWidth,
+                (float)ViewportHeight / GameHeight
+            );
+        }
+
         public override Matrix4x4 GetScaleMatrix()
         {
-            var scaleX = (float)ViewportWidth / GameWidth;
-            var scaleY = (float)ViewportHeight / GameHeight;
-            return Matrix4x4.CreateScale(scaleX, scaleY, 1.0f);
+            return Matrix4x4.CreateScale(new Vector3(GetScale(), 1.0f));
         }
 
         public override Vector2 PointToScreen(int x, int y)
