67a7cf0416eabfe83cf5c63266609d09dec7a798
demorome <demoromestuff@gmail.com>
2026-01-19 16:32:23 -0400

    add ActiveRoomID singleton component

--- src/Components/Collections/SingletonComponents.cs
+++ src/Components/Collections/SingletonComponents.cs
@@ -7,10 +7,23 @@ using System;
 using System.Text.Json.Serialization;
 using RollAndCash.ComponentSerialization;
 
-namespace RollAndCash.Components.Singletons;
+namespace RollAndCash.Components;
 
+/// <summary>
+/// Using this instead of Components.Singletons namespace,
+/// since it's more convenient to just type out "Singletons.Something",
+/// instead of "Components.Singletons.Something". <br/>
+/// Plus, it more clearly enforces explicitly declaring 
+/// if a component was declared as a singleton.
+/// </summary>
+public static class Singletons
+{
+    //FIXME: Implement!
+    //public readonly record struct ActiveLevelStringID(? ID);
 
+    public readonly record struct ActiveRoomID(LevelRoomID ID);
 
 #if DEBUG
     public readonly record struct Editor_GlobalDebugEntity();
 #endif
+}
