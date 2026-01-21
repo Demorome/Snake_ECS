8671a600fe55fdf9f048cf5eab6ca0ed1766167f
demorome <demoromestuff@gmail.com>
2026-01-20 20:10:13 -0400

    send generated content to Content/ProcessedContent

--- ContentBuilder/ContentBuilderUI/src/Operations.cs
+++ ContentBuilder/ContentBuilderUI/src/Operations.cs
@@ -300,7 +300,7 @@ public static class Operations
         var output = new DirectoryInfo(
             Path.Combine(
                 Preferences.GameDirectoryPath, 
-                "Content"
+                "Content/ProcessedContent"
             )
         );
         var classOutput =
